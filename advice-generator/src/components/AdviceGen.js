bf15d79221a15d953b3372bf12673ea8d3d14af6
ogvitorrios <vitor.rios@outlook.com>
2024-06-14 22:21:03 -0300

    first commit

---
+++ advice-generator/src/components/AdviceGen.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+const AdviceGen = ({ onClick }) => {
+  return (
+    <button onClick={onClick} className="advice-gen">
+      Get Advice
+    </button>
+  );
+};
+
+export default AdviceGen;
+
+
---
+++ advice-generator/src/pages/index.js
@@ -0,0 +1,14 @@
+import AdviceGen from '@/components/AdviceGen.js';
+
+function Home () {
+  return(
+      <div>
+          <h1>hello world</h1>
+      <AdviceGen />
+      </div>
+  ) 
+}
+
+export default Home;
+    
+      
