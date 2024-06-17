189e14b32d0db619640479dbf68650a750ef24f1
ogvitorrios <vitor.rios@outlook.com>
2024-06-16 22:16:24 -0300

    criação de mais um componente e inicio de algumas estilizações

--- advice-generator/src/pages/index.js
+++ advice-generator/src/pages/index.js
@@ -1,14 +1,17 @@
 import AdviceGen from '@/components/AdviceGen.js';
+import AdviceDisplay from '@/components/AdviceDisplay.js';
 
 function Home () {
   return(
       <div>
           <h1>hello world</h1>
       <AdviceGen />
+      <AdviceDisplay />
       </div>
   ) 
 }
 
 export default Home;
+
     
       
